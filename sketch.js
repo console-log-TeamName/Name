cb22b46d652f375b53633be812093a2920ff8c1f
Veronika <veronika.yepisheva16@gmail.com>
2024-09-25 01:09:34 +0100

    veronika's first frame - need changes

--- sketch.js
+++ sketch.js
@@ -1,62 +1,107 @@
-let y=150;
-let radius=20;
-let move = true;
+let sizeT=[1,1,1,1,1,1];
+let yT=[155, 180, 210, 100, 100, 100];
+let xT=[38, 100, 160, 246, 296, 339];
+let sizeB=[100,100,100, 100, 100, 100];
+let moveX=0;
 function setup() {
   createCanvas(400, 400);
-  
-}
-function steam(){
-  y--;
-  ellipse(37, y, radius, radius);
-  if(y+radius<0){
-    move = false;
-  }
-  if(move==false){
-    y = 150;
-    move=true;
-  }
-}
-function draw() {
-  background(225);
-  steam();
-  stroke("black");
-  line(0, 300, 400, 300);
-  rect(25, 100, 25, 200);
-  beginShape();
-  vertex(145, 150);
-  vertex(175, 150);
-  vertex(190, 300);
-  vertex(135, 300);
-  endShape(CLOSE);
-  beginShape();
-  vertex(155, 150);
-  vertex(165, 150);
-  vertex(180, 300);
-  vertex(145, 300);
-  endShape(CLOSE);
-  beginShape();
-  vertex(75, 125);
-  vertex(125, 125);
-  vertex(150, 300);
-  vertex(50, 300);
-  endShape(CLOSE);
-  beginShape();
-  vertex(90, 125);
-  vertex(110, 125);
-  vertex(135, 300);
-  vertex(65, 300);
-  endShape(CLOSE);
-  rect(200, 250, 200, 50);
-  beginShape();
-  vertex(225, 200);
-  vertex(375, 200);
-  vertex(400, 250);
-  vertex(200, 250);
-  endShape(CLOSE);
-  line(213, 225, 388, 225);
-  rect(283, 50, 25, 150);
-  rect(240, 50, 25, 150);
-  rect(326, 50, 25, 150);
-  
+  frameRate(5);
+
 }
 
+    
+  
+  function drawCloud(x, y, size) {
+    noStroke();
+    fill("grey");
+    ellipse(x, y, size, size *2);
+    ellipse(x - size/4, y, size,);
+    ellipse(x + size/4, y, size, size);
+    ellipse(x - size/2, y - size/2, size, size);
+    ellipse(x + size/2, y - size/2, size, size);
+    ellipse(x, y-size, size, size);
+  }
+  function drawCloudBig(x, y, size){
+    noStroke();
+    fill("grey");
+    ellipse(x, y, size*2, size/2);
+    ellipse(x + size/2, y+size/4, size, size/2);
+    ellipse(x - size/2, y+size/4, size, size/2);
+    ellipse(x+size, y, size, size/2); 
+    ellipse(x-size, y, size, size/2);
+    ellipse(x, y-size/4,size*1.5,size/2);
+  }
+
+function draw() {
+  
+  background(225);
+  
+  drawCloudBig(200+moveX,50,100)
+  for(let i = 0; i<xT.length; i++){
+    if(sizeT[i]<50){
+      yT[i]-=4;
+      sizeT[i]+=3;
+      drawCloud(xT[i]+moveX,yT[i],sizeT[i]);
+    }else{
+      yT[i]-=4;
+      drawCloud(xT[i]+moveX,yT[i],50);
+      if(sizeB[i]<175){
+        sizeB[i]+=2;
+        drawCloudBig(200+moveX,50,sizeB[i])
+      }else{
+        drawCloudBig(200+moveX,50,sizeB[i])
+        moveX-=2
+      }
+    }
+  }
+  
+  
+  stroke("black");
+  fill("black")
+  rect(0+moveX,350,400,50);
+  fill("brown");
+  rect(25+moveX, 150, 25, 200);
+  fill("grey");
+  beginShape();
+  vertex(145+moveX, 200);
+  vertex(175+moveX, 200);
+  vertex(190+moveX, 350);
+  vertex(135+moveX, 350);
+  endShape(CLOSE);
+  fill("silver");
+  beginShape();
+  vertex(155+moveX, 200);
+  vertex(165+moveX, 200);
+  vertex(180+moveX, 350);
+  vertex(145+moveX, 350);
+  endShape(CLOSE);
+  fill("grey");
+  beginShape();
+  vertex(75+moveX, 175);
+  vertex(125+moveX, 175);
+  vertex(150+moveX, 350);
+  vertex(50+moveX, 350);
+  endShape(CLOSE);
+  fill("silver");
+  beginShape();
+  vertex(90+moveX, 175);
+  vertex(110+moveX, 175);
+  vertex(135+moveX, 350);
+  vertex(65+moveX, 350);
+  endShape(CLOSE);
+  fill("gold");
+  rect(200+moveX, 300, 200, 50);
+  fill("red");
+  beginShape();
+  vertex(225+moveX, 250);
+  vertex(375+moveX, 250);
+  vertex(400+moveX, 300);
+  vertex(200+moveX, 300);
+  endShape(CLOSE);
+  line(213+moveX, 275, 388+moveX, 275);
+  fill("brown")
+  rect(283+moveX, 100, 25, 150);
+  rect(240+moveX, 100, 25, 150);
+  rect(326+moveX, 100, 25, 150);
+  
+}
